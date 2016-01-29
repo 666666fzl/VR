f279ad7f78ee96b9d16e04b08d7002d89fa7f6c0
Zheng <hzheng19@illinois.edu>
2016-01-29 15:34:54 -0600

    hw1

---
+++ hw1p2/Assets/Lightswitch.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lightswitch : MonoBehaviour {
+    public Light myLight;
+	// Use this for initialization
+	void Start () {
+        myLight = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyUp(KeyCode.Tab)) {
+            myLight.enabled = !myLight.enabled;
+        }
+	}
+}
+
